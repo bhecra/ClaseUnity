07dd831ee1570172f30a40ac79db6b73d323ea79
bhecra <ingchristianburbano@gmail.com>
2019-01-10 20:55:51 -0500

    WebRequest

---
+++ Assets/OVA/Scripts/WebRequest/Swapi_GET.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class Swapi_GET : MonoBehaviour {
+
+    public InputField url;
+    public Text response;
+
+    private string filmsTxt;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator GetPeople()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url.text);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }else
+        {
+            JSONNode datos = JSON.Parse(www.downloadHandler.text);
+            Debug.Log(www.downloadHandler.text.GetType());
+            //response.text = www.downloadHandler.text;
+
+            for (int i=0; i <= 2; i++ )
+            {
+                filmsTxt += datos["films"][i] + "\n";
+            }
+            //response.text = (datos["films"][0]);
+            
+            response.text = filmsTxt;
+            
+        }
+    }
+
+    public void SendRequest()
+    {
+        StartCoroutine(GetPeople());
+    }
+}
---
+++ Assets/OVA/Scripts/WebRequest/prueba1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class prueba1 : MonoBehaviour {
+
+    public Texture myTexture;
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+        //StartCoroutine(GetTexture());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://swapi.co/api/people/");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            JSONNode people = JSON.Parse(www.downloadHandler.text);
+            Debug.Log(people[0]);
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("http://www.my-server.com/image.png");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            Debug.Log("Acabe");
+        }
+    }
+}
